24d75f70437124d2a25f1c9aabfc9af2e941816d
Mahfouz <mafz060706@gmail.com>
2024-01-28 14:26:29 +0100

    Functional buttons and functional display area
    The buttons are diplayed in the display area when clicked

--- script.js
+++ script.js
@@ -31,3 +31,27 @@ function operate(symbol,firstDigit,secondDigit) {
         divide(firstDigit,secondDigit)
     }
 }
+
+
+let display = document.querySelector('#display')
+let numbers = document.querySelectorAll('.numbers')
+let addtion = document.querySelector('#add')
+let subtraction = document.querySelector('#subtract')
+let multiplication = document.querySelector('#multiply')
+let division = document.querySelector('#divide')
+let dot = document.querySelector('#dot')
+let currentDisplay = display.textContent
+
+
+function getNumbersAndSigns() {
+    numbers.forEach((number) => {
+        number.addEventListener('click', () => {display.textContent += number.textContent})
+    })
+    addtion.addEventListener('click', () => {display.textContent += ` ${addtion.textContent} `})
+    subtraction.addEventListener('click', () => {display.textContent += ` ${subtraction.textContent} `})
+    multiplication.addEventListener('click', () => {display.textContent += ` ${multiplication.textContent} `})
+    division.addEventListener('click', () => {display.textContent += ` ${division.textContent} `})
+    dot.addEventListener('click', () => {display.textContent += ` ${dot.textContent} `})
+}
+
+getNumbersAndSigns()
